10a9cd490a43381deb5904b24abf779470e80fa8
shreyansh-1911 <shreyansh19112004@gmail.com>
2025-05-16 19:38:13 +0530

    scaneer cors connection

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: ['https://smart-mess-coral.vercel.app', 'https://smart-mess-scanner.vercel.app/', 'https://smart-mess-admin.vercel.app'] }));
+app.use(cors({ origin: ['https://smart-mess-coral.vercel.app', 'https://smart-mess-scanner.vercel.app', 'https://smart-mess-admin.vercel.app'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
